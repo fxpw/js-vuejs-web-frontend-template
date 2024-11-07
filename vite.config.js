9864ac40d369c5ade4b1b3abf58a090ae6294371
fxpw <fxpw@outlook.com>
2024-11-08 01:56:52 +0300

    test

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [vue()],
+	server: {
+		//host: '0.0.0.0',
+		hmr: {
+			host: 'localhost',
+			// port: 5173
+		},
+	},
+	resolve: {
+		alias: {
+			vue: 'vue/dist/vue.esm-bundler.js',
+		},
+	},
+})
